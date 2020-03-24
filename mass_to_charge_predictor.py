db021b92323077bb86c834474b0e61e6df0eef43
Joshua Fagan <jfagan2@vols.utk.edu>
2020-03-24 10:10:15 -0400

    Refine code structure

--- mass_to_charge_predictor.py
+++ mass_to_charge_predictor.py
@@ -4,17 +4,17 @@ import argparse
 def parse_arguments():
     parser = argparse.ArgumentParser( description='Select building blocks \
                                                    to fit required mass.' )
+    group = parser.add_mutually_exclusive_group()
 
-
-    parser.add_argument( '-b', '--building_blocks', nargs='+',
+    group.add_argument( '-b', '--building_blocks', nargs='+',
                          help='List of building blocks that compose the \
                                product.' )
 
     parser.add_argument( '-q', '--quantities', type=int, nargs='+',
-                         help='List of quantities of building blocksthat \
+                         help='List of quantities of building blocks that \
                                compose the product.' )
 
-    parser.add_argument( '-b_file', '--building_block_file', nargs=1,
+    group.add_argument( '-b_file', '--building_block_file', nargs=1,
                          help='Location of file that containes building \
                                blocks and their quantities.' )
 
@@ -30,6 +30,8 @@ def parse_arguments():
         (not args.building_blocks and args.quantities)):
         parser.error('The following arguments are mutually inclusive: \
                       building_blocks, quantities')
+
+    print( args )
 
 
 def main():
